3e2b5dfec5ae840896cb17504cc455ef823a3156
IanStroemkjaerJensen <1089449@ucn.dk>
2024-05-16 21:26:37 +0200

    SuggestionsCard and first component within and displayed on the suggestions page

---
+++ app/(components)/SuggestionCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DeleteButton from "./DeleteButton";
+
+const SuggestionCard = () => {
+  return (
+    <div>
+      <DeleteButton></DeleteButton>
+    </div>
+  );
+};
+
+export default SuggestionCard;
--- app/SuggestionPage/[id]/page.jsx
+++ app/SuggestionPage/[id]/page.jsx
@@ -1,7 +1,15 @@
+import SuggestionCard from "@/app/(components)/SuggestionCard";
 import React from "react";
 
 const SuggestionPage = ({ params }) => {
-  return <div>SuggestionPage {params.id}</div>;
+  return (
+    <div>
+      Suggestions {params.id}
+      <SuggestionCard />
+      <SuggestionCard />
+      <SuggestionCard />
+    </div>
+  );
 };
 
 export default SuggestionPage;
