9534e11e7f215b66ece00e03d01719470bbfb84a
IanStroemkjaerJensen <1089449@ucn.dk>
2024-05-19 20:51:06 +0200

    Upvotes and popularity icon indicators layout is done + Posting suggestion objects to database works

--- app/(components)/VotingButton.jsx
+++ app/(components)/VotingButton.jsx
@@ -1,19 +1,22 @@
-import { faArrowUp, faArrowsUpDown } from "@fortawesome/free-solid-svg-icons";
+import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const VotingButton = () => {
   return (
-    <div className="flex flex-col">
-      <button className="mb-1">
+    <div className="flex flex-col items-center space-y-1">
+      <button>
         <FontAwesomeIcon
           icon={faArrowUp}
           className="text-orange-400 hover:cursor-pointer hover:text-red-200"
         />
       </button>
+      <label>5</label>
+      <label>Vote</label>
+      <label>0</label>
       <button>
         <FontAwesomeIcon
-          icon={faArrowsUpDown}
+          icon={faArrowDown}
           className="text-orange-200 hover:cursor-pointer hover:text-red-100"
         />
       </button>
--- app/(components)/VotingCard.jsx
+++ app/(components)/VotingCard.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import VotingButton from "./VotingButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFire } from "@fortawesome/free-solid-svg-icons";
+import UpvotesBlock from "./UpvotesBlock";
 
 const VotingCard = ({ children }) => {
   return (
-    <div className="flex p-1 m-1">
+    <div className="relative flex p-1 m-1">
       <div className="flex-1">{children}</div>
       <div className="flex flex-col justify-center ml-2">
         <VotingButton />
+      </div>
+      <div className="absolute bottom-0 left-[60%] transform translate-y-[70%] flex items-center">
+        <UpvotesBlock />
       </div>
     </div>
   );
