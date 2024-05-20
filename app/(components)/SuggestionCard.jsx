3a5eb1904e38a2457a3d85ff92e22c3fc41ea542
IanStroemkjaerJensen <1089449@ucn.dk>
2024-05-21 01:13:00 +0200

    Some styling changes to try to fix uneven design

--- app/(components)/SuggestionCard.jsx
+++ app/(components)/SuggestionCard.jsx
@@ -6,13 +6,17 @@ import VotingCard from "./VotingCard";
 const SuggestionCard = ({ suggestion }) => {
   return (
     <VotingCard suggestion={suggestion}>
-      <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2 w-fit">
+      <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
         <div className="flex mb-3 ml-auto">
           <PopularityDisplay upvotes={suggestion.upvotes} />
         </div>
-        <h4 className="mb-2">{suggestion.title}</h4>
+        <h4 className="mb-2 h-20 w-25 flex-no-wrap  overflow-hidden text-ellipsis">
+          {suggestion.title}
+        </h4>
         <hr className="h-px border-0 bg-page mb-2"></hr>
-        <p className="whitespace-pre-wrap mb-2">{suggestion.description}</p>
+        <p className="flex-grow h-40 w-25 overflow-hidden text-ellipsis mb-2">
+          {suggestion.description}
+        </p>
         <div className="flex flex-row">
           <Label labels={suggestion.labels} />
         </div>
