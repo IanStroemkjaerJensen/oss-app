f346493dff9ae52e85ef42fc5e00f909e85a1fcf
IanStroemkjaerJensen <1089449@ucn.dk>
2024-05-28 23:31:41 +0200

    Login done and working with Auth0 as provider - Todo: Get conditional render of Login button and Logout button working

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,8 @@ import { Inter } from "next/font/google";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+
 config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,14 +19,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="flex flex-col h-screen max-h-screen">
-          <Nav />
-          <div className="flex-grow overflow-y-auto bg-page text-default-textcolor">
-            {children}
+      <UserProvider>
+        <body className={inter.className}>
+          <div className="flex flex-col h-screen max-h-screen">
+            <Nav />
+            <div className="flex-grow overflow-y-auto bg-page text-default-textcolor">
+              {children}
+            </div>
           </div>
-        </div>
-      </body>
+        </body>
+      </UserProvider>
     </html>
   );
 }
