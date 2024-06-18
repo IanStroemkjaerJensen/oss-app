ef98470ee4b2ec6cfc0ecf4df4d54fadfa1bd2ea
IanStroemkjaerJensen <1089449@ucn.dk>
2024-06-18 20:55:04 +0200

    Homepage renders with Auth0 now even when no user is currently logged in yet. Also Login and logout button is now rendered conditonally with ternary operators base on if a user is logged in or not

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,17 +1,17 @@
+"use client";
+
 import { useUser } from "@auth0/nextjs-auth0/client";
 
-import { getSession, isLoading, error } from "@auth0/nextjs-auth0";
 import { faHome, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import React from "react";
 
-const Nav = async () => {
-  const { user } = await getSession();
+const Nav = () => {
+  const { user, error, isLoading } = useUser();
+
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
-  console.log(user.email_verified);
-
   return (
     <nav className="flex justify-between bg-nav p-2">
       <div className="flex items-center space-x-4">
@@ -23,17 +23,27 @@ const Nav = async () => {
         </Link>
       </div>
       <div className="flex flex-col">
-        {user &&
-          (user.email_verified ? (
-            <a className="text-white" href="/api/auth/logout">
-              Logout
-            </a>
-          ) : (
+        {user ? (
+          <>
+            {user.email_verified ? (
+              <a className="text-white" href="/api/auth/logout">
+                Logout
+              </a>
+            ) : (
+              <a className="text-white" href="/api/auth/login">
+                Login
+              </a>
+            )}
+            <p>Logged in as {user.nickname}</p>
+          </>
+        ) : (
+          <>
             <a className="text-white" href="/api/auth/login">
               Login
             </a>
-          ))}
-        <p>Logged in as {user.nickname}</p>
+            <div>No user is logged in</div>
+          </>
+        )}
       </div>
     </nav>
   );
